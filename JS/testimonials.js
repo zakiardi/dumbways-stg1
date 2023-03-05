5b4afa34984e5dabba437bd51ca41100d3f79f1d
zakiardi <muhammadzakiardianto@gmail.com>
2023-03-06 01:28:42 +0700

    week2

--- JS/testimonials.js
+++ JS/testimonials.js
@@ -1,23 +1,46 @@
-class Testimoni {
-    constructor(review, image) {
-    this._review = review
-    this._image = image
+fetch("https://api.npoint.io/da6d0233ad5457091a77")
+  .then((response) => response.json())
+  .then((data) => {
+    testimonialData = data;
+    allTestimonials();
+  })
+  .catch((error) => console.error(error));
+
+function allTestimonials() {
+  let testimonialHTML = "";
+
+  testimonialData.forEach((item) => {
+    testimonialHTML += `<div style="border: 1px #fff solid; width: 20rem; padding: 10px; margin: 10px; background-color: #fff; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);">
+                     <img style="width:full; height:10rem; object-fit: cover;" src="${item.image}" class="profile" />
+                     <p class="review">"${item.review}"</p>
+                     <p class="author">- ${item.author}</p>
+                     <p class="author">${item.rating} <i class="fa-solid fa-star"></i></p>
+                 </div>`;
+  });
+  document.getElementById("testimonials").innerHTML = testimonialHTML;
 }
 
-get review() {
-    return this._review
-}
+allTestimonials();
 
-get image() {
-    return this._image
-}
+function filterTestimonials(rating) {
+  let testimonialHTML = "";
 
-get html(){
-    return `<div class="testimoni">
-        <img src="${this._image}" class="profile" />
-        <p class="review">"${this._review}"</p>
-        <p class="author">-${this._author}"</p>
-    </div>`
- }
-}
+  const testimonialFiltered = testimonialData.filter((item) => {
+    return item.rating === rating;
+  });
 
+  if (testimonialFiltered.length === 0) {
+    testimonialHTML = `<h1> Data not found! </h1>`;
+  } else {
+    testimonialFiltered.forEach((item) => {
+      testimonialHTML += `<div style="border: 1px #fff solid; width: 20rem; padding: 10px; margin: 10px; background-color: #fff; box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);"">
+                <img style="width:full; height:10rem; object-fit: cover;" src="${item.image}" class="profile" src="${item.image}" class="profile" />
+                <p class="review">"${item.review}"</p>
+                <p class="author">- ${item.author}</p>
+                <p class="author">${item.rating} <i class="fa-solid fa-star"></i></p>
+            </div>`;
+    });
+  }
+
+  document.getElementById("testimonials").innerHTML = testimonialHTML;
+}
